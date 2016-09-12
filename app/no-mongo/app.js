972293b7a5aeb8ee58a13ee62575feaa4167192b
Alexander <alexander.zybin@gmail.com>
2016-09-12 21:54:22 +0300

    201609122154

--- app/no-mongo/app.js
+++ app/no-mongo/app.js
@@ -7,14 +7,6 @@ function _(azbn) {
 	var log_tag = 'app_js';
 	azbn.echo('Handler loaded', log_tag);
 	
-	//azbn.mdl('express').get('/',				(new require(cfg.path.app + '/route/main/index/get')(azbn)));
-	/*azbn.mdl('express').get('/',				function(req, res){
-		res.sendFile(__dirname + '/' + azbn.mdl('cfg').path.static + '/index.html');
-	});*/
-	//azbn.mdl('express').post('/',				(new require(cfg.path.app + '/route/main/index/post')(azbn)));
-	//azbn.mdl('express').put('/',				(new require(cfg.path.app + '/route/main/index/put')(azbn)));
-	//azbn.mdl('express').delete('/',				(new require(cfg.path.app + '/route/main/index/delete')(azbn)));
-	
 	//azbn.mdl('express').get('/api/call/',				(new require(cfg.path.app + '/route/api/get')(azbn)));
 	//azbn.mdl('express').post('/api/call/',				(new require(cfg.path.app + '/route/api/post')(azbn)));
 	
