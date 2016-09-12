6bc9403744f48c272fe82cae7f39cbcab4226ccf
Alexander <alexander.zybin@gmail.com>
2016-09-12 07:40:35 +0300

    201609120740

--- app/no-mongo/route/entity/item.js
+++ app/no-mongo/route/entity/item.js
@@ -9,19 +9,9 @@ function _(azbn) {
 	
 	this.handler = function(req, res) {
 		
-		return azbn.mdl('db').Entity.findById(req.params.id, function (err, entity) {
-			if(!entity) {
-				res.statusCode = 404;
-				return res.send({ error: 'Not found' });
-			}
-			if (!err) {
-				return res.send({ status: 'OK', entity : entity });
-			} else {
-				res.statusCode = 500;
-				azbn.mdl('winston').error('Internal error(%d): %s', res.statusCode, err.message);
-				return res.send({ error: 'Server error' });
-			}
-		});
+		var _p = req.params;
+		
+		return res.send({ id : _p.id });
 		
 	}
 	
--- index-no-mongo.js
+++ index-no-mongo.js
@@ -88,17 +88,8 @@ azbn.mdl('express').use(express.static(cfg.path.static, {
 
 
 
+require(cfg.path.app + '/app.js')(azbn);
 
-//azbn.mdl('express').get('/',				(new require(cfg.path.app + '/route/main/index/get')(azbn)));
-/*azbn.mdl('express').get('/',				function(req, res){
-	res.sendFile(__dirname + '/' + azbn.mdl('cfg').path.static + '/index.html');
-});*/
-//azbn.mdl('express').post('/',				(new require(cfg.path.app + '/route/main/index/post')(azbn)));
-//azbn.mdl('express').put('/',				(new require(cfg.path.app + '/route/main/index/put')(azbn)));
-//azbn.mdl('express').delete('/',				(new require(cfg.path.app + '/route/main/index/delete')(azbn)));
-
-azbn.mdl('express').get('/api/call/',				(new require(cfg.path.app + '/route/api/get')(azbn)));
-azbn.mdl('express').post('/api/call/',				(new require(cfg.path.app + '/route/api/post')(azbn)));
 
 
 // ошибки
