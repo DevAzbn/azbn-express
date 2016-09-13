1d52b3388fc43877ba6efed17342b5e8d0ffe2fe
Alexander <alexander.zybin@gmail.com>
2016-09-13 17:43:34 +0300

    201609131743

--- app/no-mongo/app.js
+++ app/no-mongo/app.js
@@ -28,6 +28,10 @@ function _(azbn) {
 	
 	azbn.mdl('express').get('/entity/list/',						(new require('./route/entity/list/get')(azbn)));
 	
+	/*
+	подключение других модулей?
+	*/
+	
 }
 
 module.exports = _;
